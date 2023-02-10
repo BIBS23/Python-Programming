23cd7feb11504ae3684c4c029760cf31fb91d749
BIBIN JOSE <83808936+BIBS23@users.noreply.github.com>
2023-02-10 09:53:49 +0530

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,7 @@
+import math
+
+radius = int(input('enter the radius of circle : '))
+area = math.pi*radius*radius
+circumferance = 2*math.pi*radius
+print('Area of circle with radius',radius, 'is  :',area)
+print('Circumferance of the circle with radius ',radius, 'is :- ',circumferance)
