e4e2386f0cc1e91eb40bcdb4a53eeea9e933c126
BIBIN JOSE <83808936+BIBS23@users.noreply.github.com>
2023-03-07 10:24:29 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,2 +1,7 @@
 list_city = ['erattupetta','thodupuzha','kottayam','eranakulam']
 print(list_city[0],list_city[3])
+
+
+list = [[1,2],[5]]
+print(list[1][0])
+print('no of elements in the list ',len(list))
