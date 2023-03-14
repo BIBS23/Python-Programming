0b36e319d98f12f7260eb9986aa55d8bdd49d824
BIBIN JOSE <83808936+BIBS23@users.noreply.github.com>
2023-03-13 20:43:08 -0800

    Update pos_neg.py

--- pos_neg.py
+++ pos_neg.py
@@ -1,14 +1,41 @@
-ListA = []
-intList = []
+
+posList = []
 negList = []
-l = int(input('enter length'))
+
+l = int(input('enter the length'))
+
+f = open("num","w")
+
 for i in range(l):
-    w = input()
-    ListA.append(w)
-for i in ListA:
-    if(int(i)>0):
-        intList.append(i)
-    else:
-        negList.append(i)
-print('List  of positive integers is :',intList)
-print('List  of negative integers is :',negList)
+  
+  i=i+1
+  
+  print('enter value',i)
+  
+  numbers = input()
+  
+  f.write(numbers+"\n")
+  
+print('file created')
+f.close()
+
+f2 = open("num","r")
+
+for i in f2:
+  
+  i = i.strip()
+  number=int(i)
+   
+  if(number>0):
+    
+    posList.append(number)
+    
+  else:
+    
+    negList.append(number)
+    
+print('positive numbers is :',posList)
+
+print('negative numbers is :',negList)
+
+f2.close()
