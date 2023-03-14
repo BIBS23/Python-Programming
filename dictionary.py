2fec74cd7fb345293be85f26cebe33be57e89184
BIBIN JOSE <83808936+BIBS23@users.noreply.github.com>
2023-03-13 21:20:54 -0800

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -4,4 +4,21 @@ info['branch'] = 'cse'
 info['branch'] = 'ece' # it will update the branch
 info.pop('name') # it will pop the name
 info['name'] = 'sandy' # name will be added 
-print(info)
+print(info) 
+for i in info:
+    print(i,info[i]) # it will print the whole key and value
+    
+    
+ # to print the dictionary as a list
+grades = {90:'A',80:"B",70:"C"}
+print(list(grades.items()))
+
+# to print as a tuple
+
+for (key,value) in grades.items():
+    print(key,value)
+    
+   
+thekeys = list(info.keys()) # it will print the keys
+thekeys.sort() # it will sort the keys
+print(thekeys)
