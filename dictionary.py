a0bacc2c718a3d621a4dae839e87876e7e5625fd
BIBIN JOSE <83808936+BIBS23@users.noreply.github.com>
2023-03-13 21:03:30 -0800

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,7 @@
 info = {}
 info['name'] = 'bibin'
 info['branch'] = 'cse'
-info['branch'] = 'ec' #to update cse
-
+info['branch'] = 'ece' # it will update the branch
+info.pop('name') # it will pop the name
+info['name'] = 'sandy' # name will be added 
 print(info)
