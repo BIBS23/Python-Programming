72d1cd0a2dbfe3055c4eafb9326981cc9e50cbe6
BIBIN JOSE <83808936+BIBS23@users.noreply.github.com>
2023-06-05 09:45:45 +0530

    Create slicing.py

---
+++ numpy/slicing.py
@@ -0,0 +1,9 @@
+a = np.array([8,3,3,4,8,9])
+b= np.array([4,3,6,7,8,9])
+
+
+print(a[0:3]) # 0 to 2 will print
+print(a[1:3]) # 1 to 2 will print
+print(a[:4]) # 0 to 3 will print
+print(a[4:]) # 4 to end will print
+print(a[-3:]) # from last to index to 3
